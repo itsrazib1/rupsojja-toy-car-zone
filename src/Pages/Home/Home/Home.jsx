b1a16530cca99e7860635436fa8c682ed94d6501
itsrazib1 <itsrazib1@gmail.com>
2023-05-21 22:57:47 +0600

    add firebase

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,7 +1,7 @@
 import About from "../About/About";
 import Product from "../Product/Product";
 import Banner from "./Banner/Banner";
-import Carosole from "./Carosole/Carosole";
+
 import Extrapart from "./Extrapart/Extrapart";
 
 
